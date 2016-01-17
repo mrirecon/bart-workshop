d07aaea02cd524924f569f228e51423543de697a
Jonathan Tamir <jtamir@eecs.berkeley.edu>
2016-01-17 01:51:12 -0800

    remove old code

--- demos/pics-phantom/demo.sh
+++ demos/pics-phantom/demo.sh
@@ -33,11 +33,21 @@ function rmcfl () {
 
 # Generate a 2D Shepp Logan phantom in k-space
 nsens=8 # coil sensitivities
-ncc=6 # coil compression
 dim=256 # ky/kz dims
-bart phantom -x $dim -k -s $nsens ksp_orig
 
-# Add noise 
+# make k-space and sensitivities separately
+bart phantom -x $dim -k ksp
+bart phantom -x $dim -S $nsens sens_orig
+bart fft -iu 7 ksp img_orig
+bart fmac img sens_orig cimg_orig
+bart fft -u 7 cimg_orig ksp_orig
+rmcfl cimg_orig
+
+# Add noise, based on SNR of k-space.
+#bart ones 1 1 o # dummy variable for summation
+#bart rss 0 ksp ksp_mag # take abs value of k-space
+#bart fmac -s `bart bitmask 0 1 2 3 4 5` ksp_mag o ksp_sum
+#noisevar=`bart show ksp_sum`
 noisevar=100
 bart noise -n $noisevar ksp_orig ksp_noise
 
@@ -62,6 +72,7 @@ cflview mask &
 # Coil-compress the data and perform an L1-ESPIRiT reconstruction
 
 # Coil compression
+ncc=6 # coil compression
 bart cc -P $ncc ksp_und ksp_und_cc
 
 # ESPIRiT sensitivity map calibration
@@ -94,30 +105,3 @@ rmcfl cimg
 # Compare the results
 bart join 2 img_noise img_zf recon_l2 recon_wav recon_tv img_compare
 cflview img_compare &
-
-
-# Coil combine
-fft -i -u 6 kSlice coil_images
-fmac -C -s 8 coil_images map im
-
-# TV denoise
-rof 20000 6 im im2
-
-# Coil split
-fmac map im2 coil_images2
-fft -u 6 coil_images2 kSlice2
-
-# Data consistency
-
-pattern kSlice pat
-
-ones 3 1 256 184 uno
-saxpy -- -1.0 pat uno antipat
-
-fmac antipat kSlice2 kSlice2_new
-saxpy 1 kSlice kSlice2_new kSlice3
-
-fft -i -u 6 kSlice3 coil_images3
-fmac -C -s 8 coil_images3 map im3
-
-
