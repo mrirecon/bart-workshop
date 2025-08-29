39301476d0ceb6ec7a29f5ac1b3d9e2b713e90c6
Vitali Telezki <vitali.telezki@med.uni-goettingen.de>
2025-08-29 16:35:49 +0200

    WIP pulseq reco

--- src/plotSubspace.py
+++ src/plotSubspace.py
@@ -10,14 +10,11 @@ def plotCoefficientMaps():
     fig, axs = plt.subplots(1,4, figsize=(12,4))
     axs =axs.ravel()
     img = absImageFromCFL("subspace_reco")
-    mask = realImageFromCFL("roi_mask")
     for i, ax in enumerate(axs):
         ax.set_xticks([])
         ax.set_yticks([])
-        roi = np.ma.masked_where(mask==0, img[:,:,i])
         ax.set_title("Coefficient # {}".format(i+1), fontdict={'fontsize':12})
-        # ax.imshow(roi, cmap=ps.createNewMap("gray"), clim=(None, 1.2))
-        ax.imshow(roi, cmap=createNewMap("gray"), clim=(None, 1.2))
+        ax.imshow(img[:,:,i], cmap=createNewMap("gray"), clim=(None, 1.2))
     plt.show()
 
 def plotTemporalEvolution():
@@ -25,13 +22,11 @@ def plotTemporalEvolution():
     axs =axs.ravel()
     timesteps=[0,100, 200,600]
     img = absImageFromCFL("imgs")
-    mask = realImageFromCFL("roi_mask")
     for i, ax in zip(timesteps,axs):
         ax.set_xticks([])
         ax.set_yticks([])
-        roi = np.ma.masked_where(mask==0, img[:,:,i])
         ax.set_title("t={}".format(i), fontdict={'fontsize':12})
-        ax.imshow(roi, cmap=createNewMap("gray"), clim=(None, 0.125))
+        ax.imshow(img[:,:,i], cmap=createNewMap("gray"), clim=(None, 0.125))
     plt.tight_layout()
     plt.show()
 
@@ -40,7 +35,7 @@ def plotT1Map():
     axs.set_xticks([])
     axs.set_yticks([])
     img =absImageFromCFL("t1map")
-    axs.imshow(img, cmap=createNewMap("gray"))
+    axs.imshow(img, cmap=createNewMap("gray"),clim=(None,1.5))
     plt.show()
 
 def createNewMap(mapName):
